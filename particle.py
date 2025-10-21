398f71ce28224e37b61d2c3ceeb3f5408f4c77ea
FY1Guo <fangyi.guo2@gmail.com>
2025-10-21 05:55:04 -0400

    add some utilities, fix bugs

--- particle.py
+++ particle.py
@@ -8,12 +8,12 @@ def sample_velocity(T, m, rng):
     Sample a velocity from the Maxwell-Boltzmann distribution at temperature T for a particle of given mass.
     """
     sigma = np.sqrt(kB * T / m)
-    velocity = np.random.normal(0, sigma, size=2) # 2D velocity
+    velocity = rng.normal(0, sigma, size=2) # 2D velocity
     return velocity
 
 
 class Particle:
-    def __init__(self, r, v, m=1.0, Lx=1.0, Ly=1.0, T=1.0, p_specular=0.6, seed=None):
+    def __init__(self, r, v, m=1.0, Lx=3.0, Ly=3.0, T=1.0, p_specular=0.6, seed=None):
         self.r = np.array(r, dtype=np.float64)  # position
         self.v = np.array(v, dtype=np.float64)  # velocity
         self.m = m  # mass
@@ -80,50 +80,68 @@ class Particle:
         # Left wall
         if x < 0.0:
             n_collisions += 1
+            vx_before = vx
             if self.rng.random() < p:
                 vx = -vx
             else:
-                vx, vy = sample_velocity(T, m, self.rng)
-                vx = abs(vx)
-            dp_sum += 2 * m * abs(vx)
+                vx_new, vy_new = sample_velocity(T, m, self.rng)
+                vx = abs(vx_new)
+                vy = vy_new
+            dp_sum += m * abs(vx - vx_before)
             x = 0.0
 
         # Right wall
         if x > self.Lx:
             n_collisions += 1
+            vx_before = vx
             if self.rng.random() < p:
                 vx = -vx
             else:
-                vx, vy = sample_velocity(T, m, self.rng)
-                vx = -abs(vx)
-            dp_sum += 2 * m * abs(vx)
+                vx_new, vy_new = sample_velocity(T, m, self.rng)
+                vx = -abs(vx_new)
+                vy = vy_new
+            dp_sum += m * abs(vx - vx_before)
             x = self.Lx
 
         # Bottom wall
         if y < 0.0:
             n_collisions += 1
+            vy_before = vy
             if self.rng.random() < p:
                 vy = -vy
             else:
-                vx, vy = sample_velocity(T, m, self.rng)
-                vy = abs(vy)
-            dp_sum += 2 * m * abs(vy)
+                vx_new, vy_new = sample_velocity(T, m, self.rng)
+                vx = vx_new
+                vy = abs(vy_new)
+            dp_sum += m * abs(vy - vy_before)
             y = 0.0
 
         # Top wall
         if y > self.Ly:
             n_collisions += 1
+            vy_before = vy
             if self.rng.random() < p:
                 vy = -vy
             else:
-                vx, vy = sample_velocity(T, m, self.rng)
-                vy = -abs(vy)
-            dp_sum += 2 * m * abs(vy)
+                vx_new, vy_new = sample_velocity(T, m, self.rng)
+                vx = vx_new
+                vy = -abs(vy_new)        # outward
+            dp_sum += m * abs(vy - vy_before)
             y = self.Ly
 
         self.r[:] = [x, y]
         self.v[:] = [vx, vy]
         return dp_sum, n_collisions
+    
+
+    def particle_interact(self, other):
+        return
+    
+
+    def step(self, dt):
+        """Perform a full time step: move + wall interaction."""
+        self.move(dt)
+        return self.wall_interact()
 
 
 class HarmonicParticle(Particle):
@@ -131,10 +149,12 @@ class HarmonicParticle(Particle):
     Particle with central-force in a 2D harmonic trap:
         V(x,y) = 1/2 kx x^2 + 1/2 ky y^2
     """
-    def __init__(self, r, v, kx=1.0, ky=1.0, **kwargs):
+    def __init__(self, r, v, kx=1.0, ky=1.0, x0=1.5, y0=1.5, **kwargs):
         super().__init__(r, v, **kwargs)
         self.kx = float(kx)
         self.ky = float(ky)
+        self.x0 = float(x0)
+        self.y0 = float(y0)
         self.omega_x = np.sqrt(self.kx / self.m)
         self.omega_y = np.sqrt(self.ky / self.m)
 
@@ -142,8 +162,10 @@ class HarmonicParticle(Particle):
     def acceleration(self, r=None):
         if r is None:
             r = self.r
-        ax = -(self.kx / self.m) * r[0]
-        ay = -(self.ky / self.m) * r[1]
+        dx = r[0] - self.x0
+        dy = r[1] - self.y0
+        ax = -(self.kx / self.m) * dx
+        ay = -(self.ky / self.m) * dy
         return np.array([ax, ay])
 
 
@@ -161,7 +183,6 @@ class HarmonicParticle(Particle):
         cy, sy = np.cos(oy*t), np.sin(oy*t)
         y = r0[1]*cy + (v0[1]/oy)*sy
         vy = -r0[1]*oy*sy + v0[1]*cy
-
         return np.array([x, y]), np.array([vx, vy])
 
 
@@ -207,9 +228,8 @@ class HardParticle(Particle):
         m1, m2 = self.m, other.m
 
         # 2D elastic collision along normal direction
-        J = (2.0 * rel_speed_n) / (1/m1 + 1/m2)
-        self.v -= (J / m1) * n
-        other.v += (J / m2) * n
+        self.v -= rel_speed_n * n
+        other.v += rel_speed_n * n
 
         # set dist to min_dist after collision to avoid overlap
         overlap = min_dist - dist
