Fix bug in pressure calculation

Particles.py
@@ -100,7 +100,7 @@ class ParticleList:
         vx_new = vx*(1-left_collisions) - vx*left_collisions*(random<p) + np.abs(diffused_vx*left_collisions*(random>=p))
         vy_new = vy*(1-left_collisions) + diffused_vy*left_collisions*(random>=p)
         
-        dp_sum += m*(vx_new-vx)
+        dp_sum += np.sum(m*(vx_new-vx))
         
         vx = vx_new.copy()
         vy = vy_new.copy()
@@ -119,7 +119,7 @@ class ParticleList:
         vx_new = vx*(1-right_collisions) - vx*right_collisions*(random<p) - np.abs(diffused_vx*right_collisions*(random>=p))
         vy_new = vy*(1-right_collisions) + diffused_vy*right_collisions*(random>=p)
         
-        dp_sum += m*(vx_new-vx)
+        dp_sum += np.sum(np.abs(m*(vx_new-vx)))
         
         vx = vx_new.copy()
         vy = vy_new.copy()
@@ -138,7 +138,7 @@ class ParticleList:
         vx_new = vx*(1-bot_collisions) + diffused_vx*bot_collisions*(random>=p)
         
         # Add to total momentum transferred to the wall
-        dp_sum += m*(vy_new - vy)
+        dp_sum += np.sum(np.abs(m*(vy_new-vy)))
         
         vx = vx_new.copy()
         vy = vy_new.copy()
@@ -158,7 +158,7 @@ class ParticleList:
         vx_new = vx*(1-bot_collisions) + diffused_vx*bot_collisions*(random>=p)
         
         # Add to total momentum transferred to the wall
-        dp_sum += m*(vy_new - vy)
+        dp_sum += np.sum(np.abs(m*(vy_new-vy)))
         
         vx = vx_new.copy()
         vy = vy_new.copy()