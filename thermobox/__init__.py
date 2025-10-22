4bd739734b148dba5c078276a7e203894aaf9324
Carter <cmatties2003@gmail.com>
2025-10-22 02:55:46 -0400

    Add functions to reproduce plots

---
+++ thermobox/__init__.py
@@ -0,0 +1,47 @@
+def harmonic_potential_plots():
+    import argparse
+    from .experiment import harmonic_equilibrium_checks
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--seed", type = int, help = "Specify a seed for random number generation")
+    
+    args = parser.parse_args()
+    
+    if args.seed is not None:
+        seed = args.seed
+    else:
+        seed = 1
+   harmonic_equilibrium_checks(seed = seed)
+   plt.show()
+
+def maxwell_boltzmann_distribution():
+    import argparse
+    from .experiment import mb_speed_histogram
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--seed", type = int, help = "Specify a seed for random number generation")
+    
+    args = parser.parse_args()
+    
+    if args.seed is not None:
+        seed = args.seed
+    else:
+        seed = 1
+   mb_speed_histogram(seed = seed)
+   plt.show()
+    
+def pressure_temp():
+    import argparse
+    from .experiment import mb_speed_histogram
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--seed", type = int, help = "Specify a seed for random number generation")
+    
+    args = parser.parse_args()
+    
+    if args.seed is not None:
+        seed = args.seed
+    else:
+        seed = 1
+   pressure_temperature(seed = seed)
+   plt.show()
